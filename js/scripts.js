c3004f638f5fdaf69a26397b2dd21e20ce362427
Dominic Brown <yeahdom@gmail.com>
2017-04-03 23:27:22 -0700

    add checkbox status checker

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ TodoItem.prototype.UpdateTodoItem = function() {
   return 0;
 }
 
-var itemArray = [];
+var itemObjArray = [];
 
 // FRONT END
 $(document).ready(function(){
@@ -19,20 +19,18 @@ $(document).ready(function(){
 
     // creat new ToDoItem object and add line to list
     var tmpObj = new TodoItem("myNewObj","True");
-    $("#output1").prepend("<li class='list-item'><input type='checkbox' ><span class='contact'>" + " " + tmpObj.name + "</span></li>");
+    $("#output1").prepend("<li class='list-item'>" +
+      "<input type='checkbox'><span class='contact'>" +
+      " " + tmpObj.name + "</span></li>");
     // update itemArray;
-    itemArray.push(tmpObj);
-    console.log("tmp obj: " , tmpObj);
+    itemObjArray.push(tmpObj);
+    // console.log("tmp obj: " , tmpObj);
 
-    $("#output1 li:first-child").click(function() {
+    $("#output1 li:first-child input").click(function() {
       //updade object
+      // yes it works using this
+      console.log("test" , $(this).is(":checked"))
 
-      // console.log("this #output1 li:first-child : " , $(this);
-        // console.log("this checked: " , this:checked);
-      // console.log("this #output1 li:first-child : " , $("this[checked='False']") );
-      // if ($(this:checked)
-      // (tmpObj.doneStatus = "False")
-      //  (tmpObj.doneStatus = "True") ;
 
     });
 
